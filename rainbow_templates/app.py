c833ccb74ebdd2a4d0923ddfd131010434de6443
Cyd-Styles <cydhaynes0@gmail.com>
2021-01-30 13:49:42 -0500

    Started the rainbow page that displays links

--- rainbow_templates/app.py
+++ rainbow_templates/app.py
@@ -3,7 +3,10 @@ from flask import Flask, render_template, current_app as app
 app = Flask(__name__)
 
 @app.route('/rainbow')
-
+def rainbow_links():
+    link = 'http://0.0.0.5000'
+    color_link = ['/red', '/orange', '/yellow', '/green', '/blue', '/indigo', '/violet']
+    return render_template('rainbow.html', link = link, color_link = color_link)
 @app.route('/red')
 def red():
     color = 'Red'

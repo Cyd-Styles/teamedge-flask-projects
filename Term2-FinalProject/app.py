cebd36089e53b26bad2f8e9505d20cd605e010be
Cyd-Styles <cydhaynes0@gmail.com>
2021-03-13 13:54:19 -0500

    Created 'message' form that prompts a login page.

---
+++ Term2-FinalProject/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for, current_app as app
+from sense_emu import SenseHat
+app = Flask(__name__)
+
+ 
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+@app.route('/success/<name>')
+def success(name):
+    return 'Welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')

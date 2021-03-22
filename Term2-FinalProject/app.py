158940786de35729ee06c1af455c2fb6575f6b3f
Cyd-Styles <cydhaynes0@gmail.com>
2021-03-22 19:57:48 -0400

    Forms work successfully, need to display message on new page{

--- Term2-FinalProject/app.py
+++ Term2-FinalProject/app.py
@@ -7,23 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template('main.html')
 
-@app.route('/success/<name>')
-def success(name):
-    return 'Welcome %s' % name
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
+@app.route('/name',methods = ['POST', 'GET'])
+def name():
    if request.method == 'POST':
       user = request.form['nm']
-      return render_template('main.html',name = user)
+      return render_template('name.html', name = user)
    else:
       user = request.args.get('nm')
-      return redirect(url_for('main.html',name = user)
+      return render_template('name.html', name = user)
 
 @app.route('/message',methods = ['POST', 'GET'])
 def message():
     if request.method == 'POST':
-        message = request.form['msg']
+        message = request.args.get('msg')
         return render_template("message_page.html", name = message)
 
 

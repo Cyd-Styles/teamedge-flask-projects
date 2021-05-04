be26386379cfa1472ba8ff848e92ec9f45a6b998
Cyd-Styles <cydhaynes0@gmail.com>
2021-05-03 20:04:43 -0400

    added jinga template

--- TasksApp/app.py
+++ TasksApp/app.py
@@ -13,24 +13,25 @@ scheduler.start()
 
 
 @app.route('/')
-def entertasks():
+def add_tasks():
     return render_template('tasks.html')
 
-@app.route('/submitted-tasks', methods=['POST'])
-def store_tasks():
-    #get posted form data using names assigned in HTML
-    subject = request.form['task-type']
-    name = request.form['task-name']
-    deadline = request.form['task-due-date']
-    #connect to database and insert subject, name, and deadline
-    conn = sqlite3.connect('./static/data/tasks-app.db')
-    curs = conn.cursor()
-    curs.execute("INSERT INTO tasks (subjects, tasknames, deadlines) VALUES((?), (?), (?))", (subject, name, deadline))
-    conn.commit()
-    #close database connection
-    conn.close()
-    #render template with success message
-    return render_template('store_tasks.html', subjects = subject, tasknames = name, deadlines = deadline)
+@app.route('/submitted-tasks', methods=['POST', 'GET'])
+def stored_tasks():
+    if request.method == 'POST':
+        #get posted form data using names assigned in HTML
+        subject = request.form['task-type']
+        name = request.form['task-name']
+        deadline = request.form['task-due-date']
+        #connect to database and insert subject, name, and deadline
+        conn = sqlite3.connect('./static/data/tasks-app.db')
+        curs = conn.cursor()
+        curs.execute("INSERT INTO tasks (subjects, tasknames, deadlines) VALUES((?), (?), (?))", (subject, name, deadline))
+        conn.commit()
+        #close database connection
+        conn.close()
+        #render template with success message
+        return render_template('store_tasks.html', subjects = subject, tasknames = name, deadlines = deadline)
 
 @app.route('/all-tasks')
 def all():
@@ -40,10 +41,12 @@ def all():
     tasks = []
     rows = curs.execute("SELECT * from tasks")
     for row in rows:
-        task = {'subject': row[0], 'names':row[1], 'deadlines':row[2]}
+        task = {'rowid': row[0], 'subject': row[1], 'names':row[2], 'deadlines':row[3]}
         tasks.append(task)
+        print(task)
     conn.close()
     return render_template('all_tasks.html', tasks = tasks)
+    
 
 
 
